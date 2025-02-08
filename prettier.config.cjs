60802d9e24a77b0b3ab9c21b6b284644498e9e45
Nikita Savenko <nikita.savenko.dev@gmail.com>
2025-02-08 12:43:56 +0200

    init

---
+++ prettier.config.cjs
@@ -0,0 +1,18 @@
+/* eslint-disable jsdoc/check-tag-names */
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+
+module.exports = {
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: false,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  endOfLine: 'auto',
+  useTabs: false,
+  plugins: ['prettier-plugin-tailwindcss'],
+}

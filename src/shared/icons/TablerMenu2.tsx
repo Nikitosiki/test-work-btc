57b508758024786e577ec30f38056f1d22cb737d
Nikita Savenko <nikita.savenko.dev@gmail.com>
2025-02-08 16:25:25 +0200

    feat: add TablerMenu2, TablerRefresh, and TablerReload icons

---
+++ src/shared/icons/TablerMenu2.tsx
@@ -0,0 +1,20 @@
+import type { SVGProps } from 'react'
+
+export function TablerMenu2(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      {...props}>
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={1}
+        d="M4 6h16M4 12h16M4 18h16"></path>
+    </svg>
+  )
+}

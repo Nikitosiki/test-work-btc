e8ad1b6dae2356cea258f43b7c88af375c99ace5
Nikita Savenko <nikita.savenko.dev@gmail.com>
2025-02-09 18:10:51 +0200

    feat: update Tailwind CSS configuration with custom colors and heroui plugin enhancements

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,24 @@
-import { heroui } from "@heroui/react";
+import { heroui } from '@heroui/react'
 
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{ts,tsx}', "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",],
+  content: [
+    './src/**/*.{ts,tsx}',
+    './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-gray': '#50627a',
+        'custom-bg': '#112135',
+        'custom-bg-light': '#18283c',
+        'custom-budge': '#fb9818',
+      },
+    },
   },
-  darkMode: "class",
-  plugins: [heroui()],
+  darkMode: 'class',
+  plugins: [
+    heroui({
+      addCommonColors: true,
+    }),
+  ],
 }
-

ad465a01537475d8c4e0f973d830801d962206df
Nikita Savenko <nikita.savenko.dev@gmail.com>
2025-02-10 13:28:45 +0200

    fix: adaptive NavigationBar

--- src/widgets/NavigationBar/NavigationBar.tsx
+++ src/widgets/NavigationBar/NavigationBar.tsx
@@ -3,10 +3,10 @@ import { FC } from 'react'
 
 import {
   TablerCoin,
-  TablerReload,
   TablerSettings,
   TablerShoppingCart,
   TablerChartHistogram,
+  TablerList,
 } from '@/shared/icons'
 
 interface NavigationBarProps {
@@ -21,7 +21,7 @@ const NavigationBar: FC<NavigationBarProps> = ({ className }) => {
       classNames={{
         base: 'bg-custom-bg-light w-full block',
         tabList: 'justify-evenly w-full',
-        tab: 'h-16',
+        tab: 'h-16 px-0',
         tabContent: 'text-custom-gray group-data-[selected=true]:text-white',
         cursor: 'hidden',
       }}
@@ -29,8 +29,8 @@ const NavigationBar: FC<NavigationBarProps> = ({ className }) => {
       <Tab
         key="dashboard"
         title={
-          <div className="flex flex-col items-center gap-1">
-            <TablerReload />
+          <div className="flex flex-col items-center max-sm:text-xs">
+            <TablerList className="sm:size-8" />
             Dashboard
           </div>
         }
@@ -38,8 +38,8 @@ const NavigationBar: FC<NavigationBarProps> = ({ className }) => {
       <Tab
         key="megabot"
         title={
-          <div className="flex flex-col items-center gap-1">
-            <TablerChartHistogram />
+          <div className="flex flex-col items-center max-sm:text-xs">
+            <TablerChartHistogram className="sm:size-8" />
             Megabot
           </div>
         }
@@ -47,30 +47,30 @@ const NavigationBar: FC<NavigationBarProps> = ({ className }) => {
       <Tab
         key="bot-market"
         title={
-          <div className="flex flex-col items-center gap-1">
-            <TablerShoppingCart />
-            Bot Market
+          <div className="flex flex-col items-center max-sm:text-xs">
+            <TablerShoppingCart className="sm:size-8" />
+            Bot market
           </div>
         }
       />
       <Tab
         key="coin-price"
         title={
-          <div className="flex flex-col items-center gap-1">
-            <TablerCoin />
-            Coin Price
+          <div className="flex flex-col items-center max-sm:text-xs">
+            <TablerCoin className="sm:size-8" />
+            Coin prices
           </div>
         }
       />
       <Tab
         key="profile"
         title={
-          <div className="flex flex-col items-center gap-1">
+          <div className="flex flex-col items-center max-sm:text-xs">
             <Badge
               content="3"
               size="md"
-              className="bg-custom-budge border-none font-semibold text-white">
-              <TablerSettings />
+              className="bg-custom-budge z-20 border-none font-semibold text-white">
+              <TablerSettings className="sm:size-8" />
             </Badge>
             Profile
           </div>

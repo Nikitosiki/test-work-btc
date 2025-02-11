4a749ec170a14a2bfb3ceba86b5585818b3b4d36
Nikita Savenko <nikita.savenko.dev@gmail.com>
2025-02-11 15:18:47 +0200

    fix: update use max-h-dvh min-h-dvh

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,30 @@
 import { HeroUIProvider } from '@heroui/react'
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 
 import Dashboard from '@/pages/Dashboard'
 import NavigationBar from '@/widgets/NavigationBar'
 import TopAppBar from '@/widgets/TopAppBar'
 
 const App: FC = () => {
+  // const [interval, setinterval] = useState('one')
+  // const [first, setfirst] = useState(localStorage.getItem('interval') || '')
+
+
+  // useEffect(() => {
+  //   setfirst(data.load(setfirst))
+  // }, [interval])
+  
+
+  // useEffect(() => {
+  //   localStorage.setItem('first', first)
+  
+  // }, [first])
+  
+
+
   return (
     <HeroUIProvider>
-      <div className="flex max-h-screen min-h-screen flex-col bg-custom-bg text-white dark">
+      <div className="flex max-h-dvh min-h-dvh flex-col bg-custom-bg text-white dark">
         <TopAppBar />
         <div className="overflow-y-auto">
           <Dashboard />
